66816f0ba1d4a67bd3b62711d78ad1f8630fb063
AbhishAnand <48917473+AbhishAnand@users.noreply.github.com>
2020-05-30 08:48:38 +0530

    Databse integration changes made in the app

--- project/main.py
+++ project/main.py
@@ -1,6 +1,6 @@
 from flask import Blueprint,render_template
 from . import db
-from flask_login import login_required
+from flask_login import login_required,current_user
 from project.models import User,Post
 
 main = Blueprint('main', __name__)
@@ -12,7 +12,7 @@ def index():
 @main.route('/profile')
 @login_required
 def profile():
-    posts=Post.query.all()
+    posts=Post.query.filter(Post.author == current_user.name).all()
     return render_template('dashboard.html',posts=posts)
 
 @main.route('/about')
